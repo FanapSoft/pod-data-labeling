c64df2effac67883864d93152146698ccb4a8b8c
Farid <Farid@DESKTOP-LOMLCSQ>
2020-07-25 13:21:27 +0430

    seperate fils

---
+++ aspnet-core/src/Fanap.DataLabeling.Application/DataSets/DataSetImportJob.cs
@@ -0,0 +1,49 @@
+﻿using Abp.BackgroundJobs;
+using Abp.Dependency;
+using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using Fanap.DataLabeling.Authorization.Users;
+using Fanap.DataLabeling.Datasets;
+using System;
+using System.IO;
+
+namespace Fanap.DataLabeling.DataSets
+{
+    public class DataSetImportJob : BackgroundJob<ImportInput>, ITransientDependency
+    {
+        private readonly IRepository<User, long> _userRepository;
+        private readonly IRepository<Dataset, Guid> dataSetRepo;
+        private readonly IRepository<DatasetItem, Guid> itemRepo;
+
+        public DataSetImportJob(IRepository<User, long> userRepository, IRepository<Dataset, Guid> dataSetRepo, IRepository<DatasetItem, Guid> itemRepo)
+        {
+            _userRepository = userRepository;
+            this.dataSetRepo = dataSetRepo;
+            this.itemRepo = itemRepo;
+        }
+
+        [UnitOfWork]
+        public override void Execute(ImportInput input)
+        {
+            var foundDateset = dataSetRepo.Get(input.DataSetId);
+            var allfiles = Directory.GetFiles(input.FolderPath, "*.*", SearchOption.AllDirectories);
+
+            foreach (var file in allfiles)
+            {
+                var fileInfo = new FileInfo(file);
+                var item = new DatasetItem();
+                item.FilePath = fileInfo.FullName;
+                item.Label = new Labels.Label
+                {
+                    Name = fileInfo.Directory.Name,
+                    DatasetId = foundDateset.Id
+                };
+                item.FileExtension = Path.GetExtension(fileInfo.FullName);
+                item.FileSize = fileInfo.Length;
+
+                itemRepo.Insert(item);
+            }
+
+        }
+    }
+}
--- aspnet-core/src/Fanap.DataLabeling.Application/DataSets/DataSetsAppService.cs
+++ aspnet-core/src/Fanap.DataLabeling.Application/DataSets/DataSetsAppService.cs
@@ -2,58 +2,16 @@
 using Abp.Application.Services.Dto;
 using Abp.Authorization;
 using Abp.BackgroundJobs;
-using Abp.Dependency;
 using Abp.Domain.Entities.Auditing;
 using Abp.Domain.Repositories;
-using Abp.Domain.Uow;
-using Fanap.DataLabeling.Authorization.Users;
 using Fanap.DataLabeling.Datasets;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fanap.DataLabeling.DataSets
 {
-    public class DataSetImportJob : BackgroundJob<ImportInput>, ITransientDependency
-    {
-        private readonly IRepository<User, long> _userRepository;
-        private readonly IRepository<Dataset, Guid> dataSetRepo;
-        private readonly IRepository<DatasetItem, Guid> itemRepo;
-
-        public DataSetImportJob(IRepository<User, long> userRepository, IRepository<Dataset, Guid> dataSetRepo, IRepository<DatasetItem, Guid> itemRepo)
-        {
-            _userRepository = userRepository;
-            this.dataSetRepo = dataSetRepo;
-            this.itemRepo = itemRepo;
-        }
-
-        [UnitOfWork]
-        public override void Execute(ImportInput input)
-        {
-            var foundDateset = dataSetRepo.Get(input.DataSetId);
-            var allfiles = Directory.GetFiles(input.FolderPath, "*.*", SearchOption.AllDirectories);
-
-            foreach (var file in allfiles)
-            {
-                var fileInfo = new FileInfo(file);
-                var item = new DatasetItem();
-                item.FilePath = fileInfo.FullName;
-                item.Label = new Labels.Label
-                {
-                    Name = fileInfo.Directory.Name,
-                    DatasetId = foundDateset.Id
-                };
-                item.FileExtension = Path.GetExtension(fileInfo.FullName);
-                item.FileSize = fileInfo.Length;
-
-                itemRepo.Insert(item);
-            }
-
-        }
-    }
-
     [AbpAuthorize]
     public class DataSetsAppService : AsyncCrudAppService<Dataset, DatasetDto, Guid>, IDataSetsAppService
     {
